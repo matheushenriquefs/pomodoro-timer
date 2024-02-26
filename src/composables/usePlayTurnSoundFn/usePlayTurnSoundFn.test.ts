8f534b9232b4846e80beaa21e9c63a8e1d5b21f3
matheushenriquefs <matheus.henrique1021@gmail.com>
2024-02-25 22:09:59 -0300

    test: add tests for usePlayTurnSoundFn

---
+++ src/composables/usePlayTurnSoundFn/usePlayTurnSoundFn.test.ts
@@ -0,0 +1,54 @@
+import { describe, it, expect, vi } from "vitest";
+import { ref } from "vue";
+import { computedEager } from "@vueuse/core";
+import { promiseTimeout } from "@vueuse/shared";
+
+import { usePlayTurnSoundFn, delay } from "./usePlayTurnSoundFn";
+
+describe("usePlayTurnSoundFn", () => {
+  it("should be defined", () => {
+    expect(usePlayTurnSoundFn).toBeDefined();
+  });
+
+  it("should play turn sfx when timer is not ticking", async () => {
+    vi.spyOn(console, "warn").mockImplementation(() => undefined);
+    let res = await usePlayTurnSoundFn(false);
+
+    expect(res).toBe(true);
+
+    await promiseTimeout(delay);
+
+    res = await usePlayTurnSoundFn(ref(false));
+
+    expect(res).toBe(true);
+
+    await promiseTimeout(delay);
+
+    res = await usePlayTurnSoundFn(computedEager(() => false));
+
+    expect(res).toBe(true);
+
+    await promiseTimeout(delay);
+  });
+
+  it("should not play turn sfx when timer is ticking", async () => {
+    vi.spyOn(console, "warn").mockImplementation(() => undefined);
+    let res = await usePlayTurnSoundFn(true);
+
+    expect(res).toBe(false);
+
+    await promiseTimeout(delay);
+
+    res = await usePlayTurnSoundFn(ref(true));
+
+    expect(res).toBe(false);
+
+    await promiseTimeout(delay);
+
+    res = await usePlayTurnSoundFn(computedEager(() => true));
+
+    expect(res).toBe(false);
+
+    await promiseTimeout(delay);
+  });
+});
